57e668492985874d5b2e816379586ee76bf7e9a6
jeremy-rutman <jeremy.rutman@gmail.com>
2024-09-11 12:42:52 -0500

    first stab

---
+++ generate_words.py
@@ -0,0 +1,41 @@
+# see http://www.bananagrammer.com/2013/10/the-boggle-cube-redesign-and-its-effect.html
+
+import random
+
+def read_dictionary(dictionaryname="scrabble_official_enable1.txt"):
+    words = []
+    with open(dictionaryname) as fp:
+        words = fp.readlines()
+        words = [word.rstrip('\n') for word in words]
+        return words
+
+def generate_boggleboard(dies):
+    '''
+    :param dies: list of dies. Each die is a length-6 list of letters/symbols
+    :return: list of die values in order, like first one is the top left , fourth is top right etc.
+    '''
+    n_dies = len(dies)
+    die_locations = [i for i in range(n_dies)] #indices for die locations
+    random.shuffle(die_locations)
+    die_values = [dies[die_location][random.randint(0,5)] for die_location in die_locations]
+    return die_values
+
+dies = [['A','A','E','E','G','N'],
+        ['A','B','B','J','O','O'],
+        ['A','C','H','O','P','S'],
+        ['A','F','F','K','P','S'],
+        ['A','O','O','T','T','W'],
+        ['C','I','M','O','T','U'],
+        ['D','E','L','R','V','Y'],
+        ['D','I','S','T','T','Y'],
+        ['E','E','G','H','N','W'],
+        ['E','E','I','N','S','U'],
+        ['E','H','R','T','V','W'],
+        ['E','I','O','S','S','T'],
+        ['E','L','R','T','T','Y'],
+        ['H','I','M','N','U','Qu'],
+        ['H','L','N','N','R','Z']]
+
+
+words = read_dictionary()
+board = generate_boggleboard(dies)
