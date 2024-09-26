06379a26b4688335806a65875747d060273c6f05
jeremy-rutman <jeremy.rutman@gmail.com>
2024-09-26 10:14:20 -0500

    hyphen now seems to be working, also converting regex matches to all possible word matches (aka 'phase 2')

--- generate_words.py
+++ generate_words.py
@@ -5,8 +5,6 @@ import numpy as np
 import re
 from typing import LiteralString
 
-from duplicity.cli_data import trans
-
 REGEX_CHARS = ['-','.','*']
 
 def read_dictionary(dictionaryname="scrabble_official_enable1.txt"):
@@ -49,8 +47,9 @@ def find_words(letter_matrix):
             print(f'words starting from {i},{j}:{words}')
             for word in words: # check for dupes. Or de-dupe at end
                 if not word in all_words:
-                    all_words.append(word)
-    return all_words
+                    all_words.append(word.lower())
+    all_with_regex = regex_matches_to_all_matches(all_words)
+    return all_with_regex
 
 
 die_used_in_word=('\0')
@@ -117,6 +116,26 @@ def is_this_a_legal_word(word_so_far):
             return True
     return False
 
+def regex_matches_to_all_matches(wordlist): # this is zach's phase 2
+    allwords = []
+    print(f'incoming wordlist:{wordlist}')
+    for word in wordlist:
+        if not (set.intersection(set(REGEX_CHARS), set(word))):  # no regex chars
+            allwords.append(word.lower())
+        regex_str = transform_to_legal_regex(word)
+        if not regex_str:  # no possible regex ilke this
+            print('this should not happen')
+        regex_str = r'^' + regex_str + r'$'
+        myregex = re.compile(regex_str)
+        for mword in LEGAL_WORDS:
+            if myregex.match(mword):
+                print(f'{mword} matches {regex_str} .')
+                if not mword in allwords:
+                    allwords.append(mword)
+    print(f'outgoing wordlist:{allwords}')
+    allwords=sorted(allwords)
+    return allwords
+
 def transform_to_legal_regex(word_so_far):
     regex_str = word_so_far
     if '-' in word_so_far:
@@ -126,21 +145,28 @@ def transform_to_legal_regex(word_so_far):
         if positions[0] == 0:
             return False
         for pos in positions: #not working for more than one -
-            if pos+1 ==len(word_so_far): #deal with trailing -
-                regex_str = regex_str[0:pos-1] + r'['+ regex_str[pos-1]+r'-' +regex_str[pos+1]+ r']'
-            else:
-                regex_str = regex_str[0:pos-1] + r'['+ regex_str[pos-1]+r'-' +regex_str[pos+1]+ r']'+regex_str[pos+2:]
-
+            if pos+1 ==len(word_so_far): #deal with trailing - kick it out for now
+                return False
+            if word_so_far[pos-1]>word_so_far[pos+1]: # deal with wraparound later W-S is illegal in meantime
+                return False
+            regex_str = regex_str[0:pos-1] + r'['+ regex_str[pos-1]+r'-' +regex_str[pos+1]+ r']'+regex_str[pos+2:]
             prev_pos = pos
     regex_str = regex_str.lower()
     return(regex_str)
 
 def can_word_start_like_this(word_so_far):
+    '''
+    todo keep list of stuff already checked to avoid checking multiple times
+    :param word_so_far:
+    :return:
+    '''
     # this check should kick out words that can't possibly begin like this
     # currently pretty inefficient  , there is prob. some smart way to do this
     # we already checked if the word itself is in the dictionary of legal words, so no need to check that
     if set.intersection(set(REGEX_CHARS),set(word_so_far)):   #
         if len(word_so_far)<3: #avoids hard matching problem, just allow everything for now
+            return True
+        if word_so_far.rfind('-')==len(word_so_far)-1: # let all trailing hyphens thru
             return True
         regex_str = transform_to_legal_regex(word_so_far)
         if not regex_str:
@@ -157,19 +183,24 @@ def can_word_start_like_this(word_so_far):
         return True
     return False
 
-def board_stats():
+def board_stats(dies):
     all_results=[]
-    dies=STANDARD_DIES
     with open('board_stats.txt', 'a') as fp:
-        fp.write('dies ')
+        fp.write('\nDies')
         for i,die in enumerate(dies):
-            fp.write(f'die {i}:')
+            fp.write(f'[')
             for s in die:
-                fp.write(f'die {i}:{s}\t')
+                fp.write(f'{s},')
+            fp.write(f']')
         fp.write('\n')
         while(1):
             board = generate_boggleboard(dies)
             print_board(board)
+            fp.write('board:')
+            for row in range(4):
+                for col in range(4):
+                    fp.write(f'{board[row][col]} ')
+                fp.write('')
             words = sorted(find_words(board))
             print(f'all words: {words}')
 
@@ -180,13 +211,9 @@ def board_stats():
             min = np.min(all_results)
             max = np.max(all_results)
             print(f'n words {n_words} avg {avg:.3} std {std:.3}')
-            fp.write(f'n_words {n_words} \tavg {avg:.3} \tstd {std:.3} \tmin {min} \tmax {max} \tN {len(all_results)} \tboard:\t')
-            for row in range(4):
-                for col in range(4):
-                    fp.write(f'{board[row][col]}\t')
-                fp.write('')
+            fp.write(f'\tn_words {n_words} \tavg {avg:.3} \tstd {std:.3} \tmin {min} \tmax {max} \tN {len(all_results)} \tboard:\twords:{words}')
             fp.write('\n')
-            fp.close()
+        fp.close()
 
 STANDARD_DIES = [
         ['A','A','E','E','G','N'],
@@ -207,7 +234,7 @@ STANDARD_DIES = [
         ['H','L','N','N','R','Z']]
 
 ALTERNATE_DIES = [
-        ['.','A','E','E','G','N'],
+        ['-','A','E','E','G','N'],
         ['A','B','B','J','O','O'],
         ['A','C','H','O','P','S'],
         ['A','F','F','K','P','S'],
@@ -225,7 +252,7 @@ ALTERNATE_DIES = [
         ['H','L','N','N','R','Z']]
 
 
-#board_stats()
+board_stats(ALTERNATE_DIES)
 #board = generate_boggleboard(STANDARD_DIES)
 board = [['R',	'I',	'E',	'L'],
     ['A',	'Qu',	'D',	'E'	],
@@ -239,7 +266,7 @@ test_hyphen = [[  'S'	,'S',	'X',	'K'],
     ['-',	'S',	'N',	'T'],
     ['W',	'V',	'G',	'T'],
     ['H',	'E',	'Z',	'W']]
-board = test_hyphen
-print_board(board)
-found_words = find_words(board)
-print(found_words)
+#board = test_hyphen
+#print_board(board)
+#found_words = find_words(board)
+#print(found_words)
